99d059cb4129230edb9aae62bfd3b3a7aa910d37
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-03-18 18:10:20 +0100

    feat(moderation): handle commentcreated

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,25 @@ import axios from "axios";
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+
+    res.send({});
+  }
+});
 
 app.listen(4003, () => {
   console.log("👮 Moderation - Listening on 4003");
