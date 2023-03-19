68afc9c80f49a8cae1c0b536ffdd57f1cba9bd02
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-03-19 19:12:37 +0100

    feat(comments): handle comment moderated

--- comments/index.js
+++ comments/index.js
@@ -37,8 +37,31 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Received event", req.body.type);
+
+  const { type, data, content } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post("http://localhost:4005", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
 });
 
 app.listen(4001, () => {
