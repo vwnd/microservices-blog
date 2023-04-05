f56fea56771db595b25143c464f5662aeca33a32
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-04-05 11:56:23 +0200

    chore(event-bus): update services addresses

--- event-bus/index.js
+++ event-bus/index.js
@@ -20,15 +20,15 @@ app.post("/events", (req, res) => {
   axios
     .post("http://posts-clusterip-service:4000/events", event)
     .catch((e) => console.log(e.message));
-  // axios
-  //   .post("http://localhost:4001/events", event)
-  //   .catch((e) => console.log(e.message));
-  // axios
-  //   .post("http://localhost:4002/events", event)
-  //   .catch((e) => console.log(e.message));
-  // axios
-  //   .post("http://localhost:4003/events", event)
-  //   .catch((e) => console.log(e.message));
+  axios
+    .post("http://comments-service:4001/events", event)
+    .catch((e) => console.log(e.message));
+  axios
+    .post("http://query-service:4002/events", event)
+    .catch((e) => console.log(e.message));
+  axios
+    .post("http://moderation-service:4003/events", event)
+    .catch((e) => console.log(e.message));
 
   res.send({ status: "OK" });
 });
